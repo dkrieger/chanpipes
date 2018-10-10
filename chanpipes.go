f66983ec0d2138276556945326c0db164a4e156a
Doug Krieger <doug@debutdigital.com>
2018-10-10 12:01:24 -0400

    add documentation for exported functions

--- chanpipes.go
+++ chanpipes.go
@@ -4,12 +4,17 @@ import (
 	"runtime"
 )
 
+// New creates a new channel and returns a read-only reference ("out") and a
+// write-only reference ("in")
 func New() (<-chan interface{}, chan<- interface{}) {
 	out := make(chan interface{})
 	in := out
 	return out, in
 }
 
+// Tee takes some readable channel as input, forwarding it to a new readable
+// channel ("out") immediately, yields to the scheduler, then forwarding it to
+// another new readable channel ("middle") after the goroutine wakes up.
 func Tee(in <-chan interface{}) (<-chan interface{}, <-chan interface{}) {
 	middle := make(chan interface{})
 	out := make(chan interface{})
@@ -22,6 +27,7 @@ func Tee(in <-chan interface{}) (<-chan interface{}, <-chan interface{}) {
 	return out, middle
 }
 
+// FanIn is like a dynamic "select" statement for N readable channels
 func FanIn(inputs ...<-chan interface{}) <-chan interface{} {
 	out := make(chan interface{})
 	for _, input := range inputs {
